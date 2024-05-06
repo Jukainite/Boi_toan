037615de88bd1a2da28d7837108c68cdd7f54279
Jukainite <114989558+Jukainite@users.noreply.github.com>
2024-05-06 13:41:20 +0700

    Update app_finger.py

--- app_finger.py
+++ app_finger.py
@@ -81,6 +81,8 @@ def preprocess_image(image_path):
 def predict_label(image_path):
     img = cv2.imread(image_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB
+    img = cv2.resize(img, (128, 128))  # Resize the image to 128x128
+    img.reshape(-1, 128, 128, 3)
     transform = transforms.Compose([
         transforms.ToTensor(),
     ])
